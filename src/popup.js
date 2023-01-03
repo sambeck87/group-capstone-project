499b4d2ce683bac6ea422c4eb612265699b8f641
Glenda Diaz <glendadiazah@outlook.com>
2023-01-03 15:47:47 -0600

    fix linter issues

--- src/popup.js
+++ src/popup.js
@@ -2,8 +2,8 @@ const pokemonAPI = 'https://pokeapi.co/api/v2/pokemon/ditto';
 const popupCont = document.querySelector('.popups-container');
 const demoButton = document.querySelector('.demo-button');
 const closePopup = () => {
-    popupCont.style.display='none';
-}
+  popupCont.style.display = 'none';
+};
 const getData = async () => {
   try {
     const result = await fetch(pokemonAPI);
@@ -13,10 +13,10 @@ const getData = async () => {
     return error;
   }
 };
-const popupWindow = async () =>{
+const popupWindow = async () => {
   const data = await getData();
   let popupGenerator = '';
-  popupGenerator=`
+  popupGenerator = `
   <div class="popup-container">
           <img class="card-img" src="${data.sprites.other.dream_world.front_default}">
           <h1 class="card-name">${data.name}</h1>
@@ -36,11 +36,11 @@ const popupWindow = async () =>{
           </ul>
           <button id="closing-button" class="closing-button"><svg class="closing-button-x" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/> </svg></button>
         </div>`;
-        popupCont.innerHTML=popupGenerator;
-        popupCont.style.display = 'flex';
-        const closingButton = document.getElementById('closing-button');
-        closingButton.addEventListener('click', closePopup);
-}
+  popupCont.innerHTML = popupGenerator;
+  popupCont.style.display = 'flex';
+  const closingButton = document.getElementById('closing-button');
+  closingButton.addEventListener('click', closePopup);
+};
 
 demoButton.addEventListener('click', popupWindow);
 
