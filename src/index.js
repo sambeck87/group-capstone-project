324712a0807475e4e63063c14742927f9169e953
sambeck87 <sandro_mipb@hotmail.com>
2023-01-03 17:19:38 -0600

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './style.css';
 import Display from './retrieve.js';
 
-// const likes = document.getElementById('pidgey');
-// console.log(likes);
+Display();
--- src/retrieve.js
+++ src/retrieve.js
@@ -2,6 +2,20 @@ const pokemonAPI = 'https://pokeapi.co/api/v2/pokemon/';
 const pokemons = ['bulbasaur', 'charmander', 'squirtle', 'caterpie', 'weedle', 'pidgey'];
 let toDisplay = '';
 
+const likesConter = async () => {
+  let poke;
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/VO75gGUDh9DCGbQH7oFL/likes')
+    .then((response) => response.json())
+    // eslint-disable-next-line
+    .then(json => poke = json);
+  for (let index = 0; index < poke.length; index += 1) {
+    const likes = document.getElementById(poke[index].item_id);
+    if (likes) {
+      likes.innerHTML = `${poke[index].likes} likes`;
+    }
+  }
+};
+
 const Display = async (pokeUrl) => {
   let pokemon;
   await fetch(pokeUrl)
@@ -19,36 +33,22 @@ const Display = async (pokeUrl) => {
   cards.innerHTML = `${toDisplay}`;
 
   const likeBtn = document.querySelectorAll('.like');
-  //console.log(likeBtn);
-  likeBtn.forEach(element => {
-    const value = element.value;
+  likeBtn.forEach((element) => {
+    const idName = element.value;
     element.addEventListener('click', async () => {
       await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/VO75gGUDh9DCGbQH7oFL/likes', {
         method: 'POST',
         body: JSON.stringify({
-          item_id: value
+          item_id: idName,
         }),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
-      })
+      });
       likesConter();
     });
   });
 
-  const likesConter = async () => {
-    let poke;
-    await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/VO75gGUDh9DCGbQH7oFL/likes')
-      .then(response => response.json())
-      // eslint-disable-next-line
-      .then(json => poke = json);
-    for (let index = 0; index < poke.length; index += 1) {
-      const likes = document.getElementById(poke[index].item_id);
-      if (likes) {
-        likes.innerHTML = `${poke[index].likes} likes`
-      }
-    }
-  }
   likesConter();
   return 0;
 };
