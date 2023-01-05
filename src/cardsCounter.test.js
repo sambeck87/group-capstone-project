c7dc4e51c6078b803cbea355a4ceef34c15f6699
sambeck87 <sandro_mipb@hotmail.com>
2023-01-05 13:15:56 -0600

    Fix linter errors

--- src/cardsCounter.test.js
+++ src/cardsCounter.test.js
@@ -6,14 +6,14 @@ const cardsCounter = require('./cardsCounter.js');
 
 // Arrange
 const Display = (pokemon) => {
-let toDisplay = '';
-for (let i = 0; i < pokemon.length; i += 1) {
-toDisplay += `<div class="cards">
+  let toDisplay = '';
+  for (let i = 0; i < pokemon.length; i += 1) {
+    toDisplay += `<div class="cards">
 <div class="features"><span>${pokemon[i].name}</span><div class="likes"><button type="button" class="like" value="${pokemon[i].name}"></button><span id="${pokemon[i].name}">likes</span></div></div>
 <button class="comments" value="${pokemon[i].name}">Comments</button>
 </div>`;
-}
-document.body.innerHTML = toDisplay;
+  }
+  document.body.innerHTML = toDisplay;
 };
 
 // Act
@@ -28,13 +28,13 @@ const test3 = cardsCounter();
 
 // Assert
 test('The number of cards should be 4', () => {
-expect(test1).toBe(4);
+  expect(test1).toBe(4);
 });
 
 test('The number of cards should be 0', () => {
-expect(test2).toBe(0);
+  expect(test2).toBe(0);
 });
 
 test('The number of cards should be 0', () => {
-expect(test3).toBe(40);
+  expect(test3).toBe(40);
 });
