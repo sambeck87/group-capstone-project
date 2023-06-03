b1373bbc4808643bf89c5f349675c0f94e0c74b0
sambeck87 <sandro_mipb@hotmail.com>
2023-06-02 19:04:59 -0600

    Change main page style Add chart

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,4 @@
+import Chart from 'chart.js/auto';
 import addComment from './addComment.js';
 import getComments from './getComments.js';
 
@@ -17,27 +18,83 @@ const popupWindow = async (pokemon) => {
     }
   };
   const data = await getData();
+
+  const power = data.stats.map((stat) => stat.stat.name);
+  const values = data.stats.map((stat) => stat.base_stat);
+
+  console.log(power, values);
+
   let popupGenerator = '';
   popupGenerator = `
   <div class="popup-container">
-          <img class="card-img" src="${data.sprites.other.dream_world.front_default}">
-          <h1 class="card-name">${data.name}</h1>
-          <ul class="card-items">
-            <li class="item"><span>Ability</span><span>${data.abilities[0].ability.name}</span></li>
-            <li class="item"><span>HP</span><span>${data.stats[0].base_stat}</span></li>
-            <li class="item"><span>Move</span><span>${data.moves[0].move.name}</span></li>
-          </ul>
-          <h2 class="comments-title">Comments</h2>
-          <form id="my-form">
-          <input id="add-user-box" type="text" class="add-user-box" placeholder="username">
-          <input id="add-comment-box" type="text" class="add-comment-box" placeholder="Add a comment">
-          <button id="add-comment-button" class="add-comment-button" value="${data.name}" type="submit">Add</button>
-          </form>
-          <ul class="comments-cont" id="popup-${data.name}" title="${data.name}">
-          </ul>
-          <button id="closing-button" class="closing-button"><svg class="closing-button-x" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/> </svg></button>
-        </div>`;
+    <div class="character">
+      <img class="card-img" src="${data.sprites.other.dream_world.front_default}">
+      <div id="radialChartContainer">
+        <canvas id="radialChart"></canvas>
+      </div>
+    </div>
+    <h1 class="card-name">${data.name}</h1>
+    <ul class="card-items">
+      <li class="item"><span>Ability</span><span>${data.abilities[0].ability.name}</span></li>
+      <li class="item"><span>HP</span><span>${data.stats[0].base_stat}</span></li>
+      <li class="item"><span>Move</span><span>${data.moves[0].move.name}</span></li>
+    </ul>
+    <h2 class="comments-title">Comments</h2>
+    <form id="my-form">
+      <input id="add-user-box" type="text" class="add-user-box" placeholder="username">
+      <input id="add-comment-box" type="text" class="add-comment-box" placeholder="Add a comment">
+      <button id="add-comment-button" class="add-comment-button" value="${data.name}" type="submit">Add</button>
+    </form>
+    <ul class="comments-cont" id="popup-${data.name}" title="${data.name}">
+    </ul>
+    <button id="closing-button" class="closing-button"><svg class="closing-button-x" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/> </svg></button>
+  </div>`;
   popupCont.innerHTML = popupGenerator;
+
+  const ctx = document.getElementById('radialChart').getContext('2d');
+  const radialChart = new Chart(ctx, {
+    type: 'radar',
+    data: {
+      labels: power,
+      datasets: [{
+        label: 'Stats',
+        data: values,
+        backgroundColor: 'rgba(248, 41, 41, 0.2)',
+        borderColor: 'rgb(248, 41, 41)',
+        borderWidth: 1,
+        fontSize: '140vw',
+      }],
+    },
+    options: {
+      scale: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      color: '#F12123',
+      size: '30vw',
+      plugins: {
+        legend: {
+          display: false,
+          labels: {
+            title: {
+              Chart: {
+                defaults: {
+                  font: {
+                    size: '100vw',
+                  },
+                },
+              },
+            },
+            font: {
+              size: '20vw', // Cambia el tamaño de letra aquí
+            },
+          },
+        },
+      },
+    },
+  });
+
   getComments(pokemon);
   popupCont.style.display = 'flex';
   const closingButton = document.getElementById('closing-button');
